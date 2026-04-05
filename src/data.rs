f67fb9061c047aa2ed21db758b55ec49aba4ec9a
jfausty <julienfausty@gmail.com>
2026-04-05 18:44:46 +0200

    feat: implement OHLC normalize mapper

--- src/data.rs
+++ src/data.rs
@@ -1,4 +1,6 @@
 use burn::data::dataset::Dataset;
+use burn::data::dataset::transform::Mapper;
+use burn::prelude::s;
 use burn::tensor::backend::Backend;
 use burn::tensor::{Tensor, TensorData};
 
@@ -93,6 +95,43 @@ impl<B: Backend> Dataset<OHLCItem<B>> for OHLCDataset<B> {
 
     fn len(&self) -> usize {
         self.loaded.shape().dims[0] - 2 * self.block_size + 1
+    }
+}
+
+pub struct NormalizeOHLCItem;
+
+impl<B: Backend> Mapper<OHLCItem<B>, OHLCItem<B>> for NormalizeOHLCItem {
+    fn map(&self, item: &OHLCItem<B>) -> OHLCItem<B> {
+        let first_timestamp = item.block.clone().slice(s![0, 0]);
+        let last_timestamp = item.block.clone().slice(s![-1, 0]);
+        let range = last_timestamp - first_timestamp.clone();
+
+        let block_times =
+            (item.block.clone().slice(s![0.., 0]) - first_timestamp.clone()).div(range.clone());
+        let next_times = (item.next.clone().slice(s![0.., 0]) - first_timestamp).div(range);
+
+        let block_vols = item.block.clone().slice(s![0.., 5]);
+        let max_vol = block_vols.clone().max().into_scalar();
+
+        let block_vols = block_vols.div_scalar(max_vol.clone());
+        let next_vols = item.next.clone().slice(s![0.., 5]).div_scalar(max_vol);
+
+        let high = item.block.clone().slice(s![0.., 4]).max().into_scalar();
+
+        let block_prices = (item.block.clone().slice(s![0.., 1..5])).div_scalar(high.clone());
+        let next_prices = (item.next.clone().slice(s![0.., 1..5])).div_scalar(high.clone());
+
+        let block = item.block.clone().zeros_like();
+        let next = item.next.clone().zeros_like();
+
+        let block = block.slice_assign(s![0.., 0], block_times);
+        let next = next.slice_assign(s![0.., 0], next_times);
+        let block = block.slice_assign(s![0.., 1..5], block_prices);
+        let next = next.slice_assign(s![0.., 1..5], next_prices);
+        let block = block.slice_assign(s![0.., 5], block_vols);
+        let next = next.slice_assign(s![0.., 5], next_vols);
+
+        OHLCItem { block, next }
     }
 }
 
@@ -272,5 +311,166 @@ mod tests {
         );
 
         assert!(dataset.get(1).is_none());
+    }
+
+    #[test]
+    fn test_normalize_simple() {
+        let device = NdArrayDevice::default();
+        let test_item = OHLCItem::<NdArray> {
+            block: Tensor::<NdArray, 2>::from_data(
+                [
+                    [0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
+                    [6.0, 7.0, 8.0, 9.0, 10.0, 11.0],
+                ],
+                &device,
+            ),
+            next: Tensor::<NdArray, 2>::from_data(
+                [
+                    [0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
+                    [6.0, 7.0, 8.0, 9.0, 10.0, 11.0],
+                ],
+                &device,
+            ),
+        };
+
+        let normalizer = NormalizeOHLCItem {};
+        let normalized = normalizer.map(&test_item);
+
+        assert!(
+            normalized
+                .block
+                .equal(Tensor::<NdArray, 2>::from_data(
+                    [
+                        [0.0, 0.1, 0.2, 0.3, 0.4, 5.0 / 11.0],
+                        [1.0, 0.7, 0.8, 0.9, 1.0, 1.0],
+                    ],
+                    &device,
+                ))
+                .all()
+                .into_scalar()
+        );
+
+        assert!(
+            normalized
+                .next
+                .equal(Tensor::<NdArray, 2>::from_data(
+                    [
+                        [0.0, 0.1, 0.2, 0.3, 0.4, 5.0 / 11.0],
+                        [1.0, 0.7, 0.8, 0.9, 1.0, 1.0],
+                    ],
+                    &device,
+                ))
+                .all()
+                .into_scalar()
+        );
+    }
+
+    #[test]
+    fn test_normalize_long() {
+        let device = NdArrayDevice::default();
+        let test_item = OHLCItem::<NdArray> {
+            block: Tensor::<NdArray, 2>::from_data(
+                [
+                    [0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
+                    [6.0, 7.0, 8.0, 9.0, 10.0, 11.0],
+                    [12.0, 13.0, 14.0, 15.0, 16.0, 17.0],
+                    [18.0, 19.0, 20.0, 21.0, 22.0, 23.0],
+                ],
+                &device,
+            ),
+            next: Tensor::<NdArray, 2>::from_data(
+                [
+                    [30.0, 31.0, 32.0, 33.0, 34.0, 35.0],
+                    [36.0, 37.0, 38.0, 39.0, 40.0, 41.0],
+                    [42.0, 43.0, 44.0, 45.0, 46.0, 47.0],
+                    [48.0, 49.0, 50.0, 51.0, 52.0, 53.0],
+                ],
+                &device,
+            ),
+        };
+
+        let normalizer = NormalizeOHLCItem {};
+        let normalized = normalizer.map(&test_item);
+
+        assert!(
+            normalized
+                .block
+                .equal(Tensor::<NdArray, 2>::from_data(
+                    [
+                        [
+                            0.0,
+                            1.0 / 22.0,
+                            2.0 / 22.0,
+                            3.0 / 22.0,
+                            4.0 / 22.0,
+                            5.0 / 23.0
+                        ],
+                        [
+                            6.0 / 18.0,
+                            7.0 / 22.0,
+                            8.0 / 22.0,
+                            9.0 / 22.0,
+                            10.0 / 22.0,
+                            11.0 / 23.0
+                        ],
+                        [
+                            12.0 / 18.0,
+                            13.0 / 22.0,
+                            14.0 / 22.0,
+                            15.0 / 22.0,
+                            16.0 / 22.0,
+                            17.0 / 23.0
+                        ],
+                        [1.0, 19.0 / 22.0, 20.0 / 22.0, 21.0 / 22.0, 1.0, 1.0],
+                    ],
+                    &device,
+                ))
+                .all()
+                .into_scalar()
+        );
+
+        assert!(
+            normalized
+                .next
+                .equal(Tensor::<NdArray, 2>::from_data(
+                    [
+                        [
+                            30.0 / 18.0,
+                            31.0 / 22.0,
+                            32.0 / 22.0,
+                            33.0 / 22.0,
+                            34.0 / 22.0,
+                            35.0 / 23.0
+                        ],
+                        [
+                            36.0 / 18.0,
+                            37.0 / 22.0,
+                            38.0 / 22.0,
+                            39.0 / 22.0,
+                            40.0 / 22.0,
+                            41.0 / 23.0
+                        ],
+                        [
+                            42.0 / 18.0,
+                            43.0 / 22.0,
+                            44.0 / 22.0,
+                            45.0 / 22.0,
+                            46.0 / 22.0,
+                            47.0 / 23.0
+                        ],
+                        [
+                            48.0 / 18.0,
+                            49.0 / 22.0,
+                            50.0 / 22.0,
+                            51.0 / 22.0,
+                            52.0 / 22.0,
+                            53.0 / 23.0
+                        ],
+                    ],
+                    &device,
+                ))
+                .all()
+                .into_scalar()
+        );
     }
 }
--- src/train.rs
+++ src/train.rs
@@ -1,15 +1,22 @@
 use burn::backend::{Wgpu, wgpu::WgpuDevice};
+use burn::data::dataset::Dataset;
+use burn::data::dataset::transform::MapperDataset;
 
 use std::io::stdin;
 
 mod data;
-use data::OHLCDataset;
+use data::{NormalizeOHLCItem, OHLCDataset, OHLCItem};
+
+type Backend = Wgpu<f32, i32>;
 
 fn main() -> Result<(), String> {
     let device = WgpuDevice::default();
-    let ohlc_dataset = OHLCDataset::<Wgpu<f32, i32>>::new(60, stdin(), &device);
+    let ohlc_dataset = OHLCDataset::<Backend>::new(64, stdin(), &device).unwrap();
 
-    println!("{:?}", ohlc_dataset);
+    let mapped_dataset: MapperDataset<_, _, OHLCItem<Backend>> =
+        MapperDataset::new(ohlc_dataset, NormalizeOHLCItem);
+
+    println!("{:?}", mapped_dataset.get(0));
 
     Ok(())
 }
