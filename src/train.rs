532aa16e3eb1900cd77c082432e5449f1333f839
jfausty <julienfausty@gmail.com>
2026-04-27 20:08:05 +0200

    feat: implement DataConfig in train executable

--- src/train.rs
+++ src/train.rs
@@ -1,4 +1,5 @@
 use burn::backend::{Wgpu, wgpu::WgpuDevice};
+use burn::config::Config;
 use burn::data::dataset::Dataset;
 use burn::data::dataset::transform::MapperDataset;
 
@@ -8,6 +9,22 @@ mod data;
 use data::{NormalizeOHLCItem, OHLCDataset, OHLCItem};
 
 type Backend = Wgpu<f32, i32>;
+
+#[derive(Config, Debug)]
+struct DataConfig {
+    #[config(default = 1.0)]
+    pub use_only: f32,
+    #[config(default = 0.8)]
+    pub train_split: f32,
+    #[config(default = 32)]
+    pub batch_size: usize,
+    #[config(default = 4)]
+    pub num_workers: usize,
+    #[config(default = 42)]
+    pub seed: u64,
+}
+
+impl DataConfig {}
 
 fn main() -> Result<(), String> {
     let device = WgpuDevice::default();
